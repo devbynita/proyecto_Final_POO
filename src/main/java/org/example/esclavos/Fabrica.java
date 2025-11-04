806b18ae68d2798304cd002c4426a1e017d027f5
Alejo <alejandro.sanchez11@eia.edu.co>
2025-11-04 14:30:24 -0500

    feaat: project advance

--- src/main/java/org/example/esclavos/Fabrica.java
+++ src/main/java/org/example/esclavos/Fabrica.java
@@ -1,11 +1,58 @@
 package org.example.esclavos;
 
 public class Fabrica {
-    private String nombre;
+    private int id;
+
     private String pais;
     private String ciudad;
     private int capacidad;
     private int nivelAutomatizacion;
 
+    public Fabrica(int id, String pais, String ciudad, int capacidad, int nivelAutomatizacion) {
+       this.id = id;
+        this.pais = pais;
+        this.ciudad = ciudad;
+        this.capacidad = capacidad;
+        this.nivelAutomatizacion = nivelAutomatizacion;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getNivelAutomatizacion() {
+        return nivelAutomatizacion;
+    }
+
+    public void setNivelAutomatizacion(int nivelAutomatizacion) {
+        this.nivelAutomatizacion = nivelAutomatizacion;
+    }
+    public void empezarJornada(){
+        System.out.println("La fabrica en " + ciudad+" ,"+pais+" ha comenzado su jornada" );
+    }
+    public void
 }
